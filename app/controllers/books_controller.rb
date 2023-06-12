8f707de7be1bc32c72ba4f85315faba78fda1b58
EC2 Default User <ec2-user@ip-172-31-42-167.ap-northeast-1.compute.internal>
2023-06-12 09:53:50 +0000

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,9 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
+    if params[:tag_name]
+      @books = Book.tagged_with("#{params[:tag_name]}").all
+    end
   end
 
   def create
@@ -41,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :tag_list)
   end
 
   def ensure_correct_user
